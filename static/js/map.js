b33cea9c5304d9a30245f28de3c963b00dc4f796
jross <jake.ross@nmt.edu>
2023-07-06 16:11:26 -0600

    added map includes

--- static/js/map.js
+++ static/js/map.js
@@ -92,7 +92,7 @@ function initMap(center, zoom, dataurl){
             source: 'wells',
             paint: {
                 'circle-radius': 4,
-                'circle-color': '#223ab4',
+                'circle-color': '#224bb4',
                 'circle-stroke-color': 'rgba(0,0,0,1)',
                 'circle-stroke-width': 1,
             }
